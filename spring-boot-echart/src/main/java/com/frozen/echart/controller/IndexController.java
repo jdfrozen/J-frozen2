ae7c0df7ff31498660b797aaa3884457be536b61
jdfrozen <616769416@qq.com>
2021-01-26 11:01:17 +0800

    修改echart

--- spring-boot-echart/src/main/java/com/frozen/echart/controller/IndexController.java
+++ spring-boot-echart/src/main/java/com/frozen/echart/controller/IndexController.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping(value = "index")
     public String index(){
-        return "/index";
+        return "index";
     }
 }
